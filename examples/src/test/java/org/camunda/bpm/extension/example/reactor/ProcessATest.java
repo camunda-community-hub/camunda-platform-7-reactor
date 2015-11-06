46240a29bd1af2d32e1d82a99814b9803870303c
Jan Galinski <jan.galinski@holisticon.de>
2015-11-06 13:51:13 +0100

    fix problem with assertj assertion on candidateGroups

--- examples/src/test/java/org/camunda/bpm/extension/example/reactor/ProcessATest.java
+++ examples/src/test/java/org/camunda/bpm/extension/example/reactor/ProcessATest.java
@@ -3,6 +3,7 @@ package org.camunda.bpm.extension.example.reactor;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.engine.test.assertions.ProcessEngineTests;
@@ -12,6 +13,8 @@ import org.junit.Test;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.taskService;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.taskQuery;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.task;
 
@@ -35,11 +38,18 @@ public class ProcessATest {
     assertThat(processInstance).isWaitingAt("task_a");
     assertThat(task()).hasDueDate(ProcessA.DUE_DATE);
     
+    
+    Task t = taskService().createTaskQuery().singleResult();
+    assertThat(taskQuery().taskId(t.getId()).taskCandidateGroup(ProcessA.GROUP_1).includeAssignedTasks().singleResult()).isNotNull();
+    assertThat(taskQuery().taskId(t.getId()).taskCandidateGroup(ProcessA.GROUP_2).includeAssignedTasks().singleResult()).isNotNull();
+    assertThat(taskQuery().taskId(t.getId()).taskCandidateGroup(ProcessA.GROUP_3).includeAssignedTasks().singleResult()).isNotNull();
+    
+    
 
-    assertThat(task()).hasCandidateGroup(ProcessA.GROUP_1);
-    assertThat(task()).hasCandidateGroup(ProcessA.GROUP_2);
-    assertThat(task()).hasCandidateGroup(ProcessA.GROUP_3);
-//    assertThat(task()).isAssignedTo("me");
+    //assertThat(task()).hasCandidateGroup(ProcessA.GROUP_1);
+    //assertThat(task()).hasCandidateGroup(ProcessA.GROUP_2);
+    //assertThat(task()).hasCandidateGroup(ProcessA.GROUP_2);
+    assertThat(task()).isAssignedTo("me");
 
     
   }
