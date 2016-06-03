68959bb07ea45923c96b4376edeabe41f80a5b68
Galinski, Jan / Kuehne + Nagel / HAM GI-DB/ external <external.jan.galinski@kuehne-nagel.com>
2016-06-03 12:08:38 +0200

    provide documentation for spring example, add eventBus to configuration

--- examples/README.md
+++ examples/README.md
@@ -3,3 +3,4 @@
 * [Task Listener for BPMN](bpmn-task-listener)
 * [Task Listener for CMMN](cmmn-task-listener)
 * [Execution Listener for BPMN](bpmn-execution-listener)
+* [Automatic plugin confi in spring boot](spring-plugin)
--- extension/spring/src/main/java/org/camunda/bpm/extension/reactor/spring/CamundaReactorConfiguration.java
+++ extension/spring/src/main/java/org/camunda/bpm/extension/reactor/spring/CamundaReactorConfiguration.java
@@ -2,8 +2,10 @@ package org.camunda.bpm.extension.reactor.spring;
 
 import org.camunda.bpm.extension.reactor.bus.CamundaEventBus;
 import org.camunda.bpm.extension.reactor.plugin.ReactorProcessEnginePlugin;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.bus.EventBus;
 
 @Configuration
 public class CamundaReactorConfiguration {
@@ -11,6 +13,12 @@ public class CamundaReactorConfiguration {
   @Bean
   public CamundaEventBus camundaEventBus() {
     return new CamundaEventBus();
+  }
+
+  @Bean
+  @Qualifier("camunda")
+  public EventBus eventBus(final CamundaEventBus camundaEventBus) {
+    return camundaEventBus.get();
   }
 
   @Bean
