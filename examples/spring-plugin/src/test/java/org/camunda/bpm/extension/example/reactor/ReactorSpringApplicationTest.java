774bdaf91a0ce9701c1e9e7989f55a40fb11e271
Jan Galinski <jan.galinski@holisticon.de>
2017-11-06 17:03:40 +0100

    move examples to profile, remove deprecated helpers

--- examples/spring-plugin/src/test/java/org/camunda/bpm/extension/example/reactor/ReactorSpringApplicationTest.java
+++ examples/spring-plugin/src/test/java/org/camunda/bpm/extension/example/reactor/ReactorSpringApplicationTest.java
@@ -10,13 +10,13 @@ import org.camunda.bpm.extension.reactor.plugin.ReactorProcessEnginePlugin;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {ReactorSpringApplication.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ReactorSpringApplication.class)
 public class ReactorSpringApplicationTest {
 
   @Autowired
