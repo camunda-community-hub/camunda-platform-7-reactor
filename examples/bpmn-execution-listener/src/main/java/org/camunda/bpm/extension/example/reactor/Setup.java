f19d5375e3dd1e95f090737f9de0723c0afadc36
wgitscht <werner@gitschthaler.net>
2017-12-14 20:46:49 +0100

    updated example (would suggest not to merge it, i suppose the "process" is not used much in the selector, just for your convenience to test it

--- examples/bpmn-execution-listener/src/main/java/org/camunda/bpm/extension/example/reactor/Setup.java
+++ examples/bpmn-execution-listener/src/main/java/org/camunda/bpm/extension/example/reactor/Setup.java
@@ -3,6 +3,7 @@ package org.camunda.bpm.extension.example.reactor;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
+import org.camunda.bpm.engine.impl.persistence.StrongUuidGenerator;
 import org.camunda.bpm.extension.reactor.CamundaReactor;
 import org.camunda.bpm.extension.reactor.bus.CamundaEventBus;
 
@@ -14,6 +15,7 @@ public class Setup {
       this.getProcessEnginePlugins().add(CamundaReactor.plugin());
       this.jobExecutorActivate = false;
       this.isDbMetricsReporterActivate = false;
+      this.idGenerator = new StrongUuidGenerator(); // should be used for production
     }
   };
 
